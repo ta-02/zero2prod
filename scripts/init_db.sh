4a261675b83bf08238f2dc1bd790c647f5071d4f
ta-02 <talal.gl2004@gmail.com>
2024-11-05 14:00:48 -0500

    updated ci/cd

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -10,7 +10,7 @@ if ! [ -x "$(command -v sqlx)" ]; then
   echo >&2 "to install it."
   exit 1
 fi
-    
+
 DB_PORT="${POSTGRES_PORT:=5432}"
 SUPERUSER="${SUPERUSER:=postgres}"
 SUPERUSER_PWD="${SUPERUSER_PWD:=password}"
@@ -19,34 +19,37 @@ APP_USER="${APP_USER:=app}"
 APP_USER_PWD="${APP_USER_PWD:=secret}"
 APP_DB_NAME="${APP_DB_NAME:=newsletter}"
 
-CONTAINER_NAME="postgres"
-docker run \
-  --env POSTGRES_USER=${SUPERUSER} \
-  --env POSTGRES_PASSWORD=${SUPERUSER_PWD} \
-  --health-cmd="pg_isready -U ${SUPERUSER} || exit 1" \
-  --health-interval=1s \
-  --health-timeout=5s \
-  --health-retries=5 \
-  --publish "${DB_PORT}":5432 \
-  --detach \
-  --name "${CONTAINER_NAME}" \
-  postgres -N 1000
+# Check if running in GitHub Actions and skip starting a Docker container if so
+if [ -z "${CI}" ]; then
+  CONTAINER_NAME="postgres"
+  docker run \
+    --env POSTGRES_USER=${SUPERUSER} \
+    --env POSTGRES_PASSWORD=${SUPERUSER_PWD} \
+    --health-cmd="pg_isready -U ${SUPERUSER} || exit 1" \
+    --health-interval=1s \
+    --health-timeout=5s \
+    --health-retries=5 \
+    --publish "${DB_PORT}":5432 \
+    --detach \
+    --name "${CONTAINER_NAME}" \
+    postgres -N 1000
 
-until [ \
-  "$(docker inspect -f "{{.State.Health.Status}}" ${CONTAINER_NAME})" == \
-  "healthy" \
-  ]; do
-    >&2 echo "Postgres is still unavailable - sleeping"
-    sleep 1
-done
+  until [ \
+    "$(docker inspect -f "{{.State.Health.Status}}" ${CONTAINER_NAME})" == \
+    "healthy" \
+    ]; do
+      >&2 echo "Postgres is still unavailable - sleeping"
+      sleep 1
+  done
+fi
 
 >&2 echo "Postgres is up and running on port ${DB_PORT}"
 
 CREATE_QUERY="CREATE USER ${APP_USER} WITH PASSWORD '${APP_USER_PWD}';"
-docker exec -it "${CONTAINER_NAME}" psql -U "${SUPERUSER}" -c "${CREATE_QUERY}"
+docker exec -it "${CONTAINER_NAME}" psql -U "${SUPERUSER}" -c "${CREATE_QUERY}" || true
 
 GRANT_QUERY="ALTER USER ${APP_USER} CREATEDB;"
-docker exec -it "${CONTAINER_NAME}" psql -U "${SUPERUSER}" -c "${GRANT_QUERY}"
+docker exec -it "${CONTAINER_NAME}" psql -U "${SUPERUSER}" -c "${GRANT_QUERY}" || true
 
 DATABASE_URL=postgres://${APP_USER}:${APP_USER_PWD}@localhost:${DB_PORT}/${APP_DB_NAME}
 export DATABASE_URL
